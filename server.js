321d1384ef8fa2c715ac025bab1eba24c150a504
tmdgjs1274-svg <tmdgjs1274@gmail.com>
2026-08-19 19:42:08 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/host', (req, res) => res.sendFile(path.join(__dirname, 'public', 'host.html')));
+app.get('/play', (req, res) => res.sendFile(path.join(__dirname, 'public', 'client.html')));
+app.get('/', (req, res) => res.redirect('/play'));
+
+io.on('connection', (socket) => {
+  socket.on('start_game', (roles) => io.emit('game_started', roles));
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
